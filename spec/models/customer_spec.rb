226a82dc43a6f12c09a72e44f382aebf5f9fbbec
Hayley Witherell <hayleywitherell@gmail.com>
2024-08-14 16:43:40 -0600

    Complete tests left

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
+
 RSpec.describe "CustomersControllers", type: :request do
+
   describe "get customers_path" do
     it "renders the index view" do
       FactoryBot.create_list(:customer, 10)
@@ -7,23 +9,35 @@ RSpec.describe "CustomersControllers", type: :request do
       expect(response).to render_template(:index)
     end
   end
+
   describe "get customer_path" do
     it "renders the :show template" do
       customer = FactoryBot.create(:customer)
       get customer_path(id: customer.id)
       expect(response).to render_template(:show)
     end
+
     it "redirects to the index path if the customer id is invalid" do
       get customer_path(id: 5000) #an ID that doesn't exist
       expect(response).to redirect_to customers_path
     end
   end
-describe "get new_customer_path" do
-    it "renders the :new template"
+
+  describe "get new_customer_path" do
+    it "renders the :new template" do
+      get new_customer_path
+      expect(response).to render_template(:new)
+    end
   end
+
   describe "get edit_customer_path" do
-    it "renders the :edit template"
+    it "renders the :edit template" do
+      customer = FactoryBot.create(:customer)
+      get edit_customer_path(customer.id)
+      expect(response).to render_template(:edit)
+    end
   end
+
   describe "post customers_path with valid data" do
     it "saves a new entry and redirects to the show path for the entry" do
       customer_attributes = FactoryBot.attributes_for(:customer)
@@ -32,6 +46,7 @@ describe "get new_customer_path" do
       expect(response).to redirect_to customer_path(id: Customer.last.id)
     end
   end
+
   describe "post customers_path with invalid data" do
     it "does not save a new entry or redirect" do
       customer_attributes = FactoryBot.attributes_for(:customer)
@@ -41,9 +56,16 @@ describe "get new_customer_path" do
       expect(response).to render_template(:new)
     end
   end
+
   describe "put customer_path with valid data" do
-    it "updates an entry and redirects to the show path for the customer"
+    it "updates and redirects" do
+      customer = FactoryBot.create(:customer)
+      put customer_path(customer.id), params: { customer: { phone: "5555555555" } }
+      expect(customer.reload.phone).to eq("5555555555")
+      expect(response).to redirect_to customer_path(customer.id)
+    end
   end
+
   describe "put customer_path with invalid data" do
     it "does not update the customer record or redirect" do
       customer = FactoryBot.create(:customer)
@@ -53,7 +75,14 @@ describe "get new_customer_path" do
       expect(response).to render_template(:edit) 
     end
   end
+
   describe "delete a customer record" do
-    it "deletes a customer record"
+    it "deletes a customer record" do
+      customer = FactoryBot.create(:customer)
+      expect {
+        delete customer_path(customer.id)
+      }.to change(Customer, :count).by(-1)
+      expect(response).to redirect_to customers_path
+    end
   end
 end
