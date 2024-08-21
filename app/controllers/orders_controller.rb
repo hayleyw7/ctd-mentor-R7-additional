488704abae7be09d534de0886de114700cfe2683
Hayley Witherell <hayleywitherell@gmail.com>
2024-08-21 17:13:05 -0600

    Make views and forms work

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,20 +16,21 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(params[:order])
+    @order = Order.new(order_params)
     if @order.save
-      redirect_to @order, notice: 'Order was successfully created.'
+      flash.notice = "The order was successfully created."
+      redirect_to @order
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
     @order = Order.find(params[:id])
-    if @order.update(params[:order])
+    if @order.update(order_params)
       redirect_to @order, notice: 'Order was successfully updated.'
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -37,5 +38,11 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order.destroy
     redirect_to orders_url, notice: 'Order was successfully destroyed.'
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :product_name, :product_count)
   end
 end
