4a577b439fb8137ae2574fbde1f8bc7c8b2aff86
Hayley Witherell <hayleywitherell@gmail.com>
2024-08-20 19:39:04 -0600

    Setup orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,41 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def create
+    @order = Order.new(params[:order])
+    if @order.save
+      redirect_to @order, notice: 'Order was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(params[:order])
+      redirect_to @order, notice: 'Order was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to orders_url, notice: 'Order was successfully destroyed.'
+  end
 end
