2b7f2269c9f65fe8790cbaa1a7241113a9603714
Hayley Witherell <hayleywitherell@gmail.com>
2024-08-31 13:55:21 -0600

    Create modal

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   get 'customers/index', to: 'customers#index', as: 'customers_index'
 
-  resources :customers
+  resources :customers do
+    member do
+      delete :destroy_with_orders
+    end
+  end
+
   resources :orders
 end
